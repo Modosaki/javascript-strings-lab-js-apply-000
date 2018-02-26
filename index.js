feb01a4cc081fd7c43e6c16cacafb92378129555
Karim Ezzat <modosaki@gmail.com>
2018-02-26 22:38:36 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,4 +6,8 @@ var greetSpecialGuest = "Hello, " + specialGuest + "!";
 
 var topic = "space";
 
+<<<<<<< HEAD
 var conversation = `Let\'s talk about ${topic}.`
+=======
+var conversation = '`${topic}`;
+>>>>>>> 58ea73bf7902d003b312796ffd026b36b06d5281
